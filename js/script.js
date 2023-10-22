6db4c71867e375cefa8d91246ca9e9d8558e86ff
inoulen inoulen <inoulenkon@gmail.com>
2023-10-23 00:26:34 +0530

    Add doctor-appointment and budget-app project cards

--- js/script.js
+++ js/script.js
@@ -33,17 +33,58 @@ const cardWorksWrapper = document.getElementById('card-works-wrapper');
 const popupCardDescription = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea";
 const projectDetails = [
   {
+    title: 'Doctor Appointment',
+    description:
+      'This is a two-app simple doctor appointment web application. This app allows keeping a record of doctor appointments by creating or deleting doctors and appointments. Built with ReactJS, Redux, Ruby on Rails, and PostgreSQL',
+    image: './images/large/doctor-appointment.png',
+    alt: 'doctor appointment image',
+    companyRoles: ['Microverse', 'Student', '2023'],
+    skills: ['ReactJS', 'Redux', 'Ruby', 'Rails', 'PostgreSQL'],
+    liveLink: { link: 'https://doctor-appointment-3o4a.onrender.com/' },
+    sourceLink: { link: 'https://github.com/abel-tefera/doctor-appointment-back-end' },
+    buttonText: 'See Project',
+    id: 1,
+  },
+  {
+    title: 'Budget App',
+    description:
+      'Budget App is a Ruby on Rails mobile web application where users can manage their budget: users have a list of transactions associated with a category, so that users can see how much money the users spent on what.',
+    image: './images/large/budget-app.png',
+    alt: 'budget app image',
+    companyRoles: ['Microverse', 'Student', '2023'],
+    skills: ['Ruby', 'Rails', 'PostgreSQL'],
+    liveLink: { link: 'https://budget-app-3e3v.onrender.com' },
+    sourceLink: { link: 'https://github.com/lenkon/Budget-App' },
+    buttonText: 'See Project',
+    id: 2,
+    flip: 1,
+  },
+  {
+    title: 'Metrics App - Community TV Data',
+    description:
+      'This React capstone project is about building a mobile web application to check a list of metrics (numeric values) that is created making use of React and Redux.',
+    image: './images/large/metrics-app.png',
+    alt: 'metrics app image',
+    companyRoles: ['Microverse', 'Student', '2023'],
+    skills: ['React', 'Redux', 'JavaScript'],
+    liveLink: { link: 'https://react-capstone-project-metrics-webapp.onrender.com/' },
+    sourceLink: { link: 'https://github.com/lenkon/react-capstone-project-metrics-webapp' },
+    buttonText: 'See Project',
+    id: 3,
+  },
+  {
     title: 'Capstone 1 - Concert Page',
     description:
       'This is a capstone project for the first module where I create a concert festival page using HTML, CSS and JavaScript.',
-    image: './images/large/concert-screenshot2.png',
+    image: './images/large/concert.png',
     alt: 'concert festival image',
     companyRoles: ['Microverse', 'Student', '2022'],
     skills: ['html', 'css', 'javascript'],
     liveLink: { link: 'https://lenkon.github.io/capstone-concert-page/' },
     sourceLink: { link: 'https://github.com/lenkon/capstone-concert-page' },
     buttonText: 'See Project',
-    id: 1,
+    id: 4,
+    flip: 1,
   },
 
   {
@@ -57,8 +98,7 @@ const projectDetails = [
     liveLink: { link: 'https://lenkon.github.io/awesome-books-es6/' },
     sourceLink: { link: 'https://github.com/lenkon/awesome-books-es6' },
     buttonText: 'See Project',
-    flip: 1,
-    id: 2,
+    id: 5,
   },
 
   {
@@ -72,7 +112,8 @@ const projectDetails = [
     liveLink: { link: 'https://lenkon.github.io/my-portfolio/' },
     sourceLink: { link: 'https://github.com/lenkon/Portfolio' },
     buttonText: 'See Project',
-    id: 3,
+    id: 5,
+    flip: 1,
   },
 ];
 
@@ -103,9 +144,11 @@ const createProjectDetails = (item) => {
         ${roles.join('')}
       </div>
       <p class="card-main-text">${item.description}</p>
-      <ul class="card-tools-list">
-        ${skillsList.join('')}
-      </ul>
+      <div class="card-tools-list">
+        <ul>
+          ${skillsList.join('')}
+        </ul>
+      </div>
       <div class="card-button-box">
         <button card-id=${
   item.id
